9f60fd2592e83842fac3a4edeaca630d088d3dbd
kenshi kamata <kenshi.kamata@gmail.com>
2018-01-17 02:29:04 +0900

    一応それっぽく動いている

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"os/signal"
 	"regexp"
 	"syscall"
+
+	"github.com/mgutz/ansi"
 )
 
 var (
@@ -53,6 +55,11 @@ func (dc *DiffContext) handleLine(input string) error {
 		dc.ShowHunk()
 		dc.Added = []string{}
 		dc.Removed = []string{}
+		b, err := regexp.MatchString("[\\@ ]", input)
+		if err != nil {
+			return err
+		}
+		dc.InHunk = b
 	}
 	return nil
 }
@@ -70,13 +77,70 @@ func isAddedLine(in string) bool {
 }
 
 func (dc *DiffContext) ShowHunk() {
-	fmt.Println("Added lines")
-	for _, v := range dc.Added {
-		fmt.Println("\t" + v)
+	if len(dc.Added) == 0 || len(dc.Removed) == 0 {
+		for _, v := range dc.Added {
+			fmt.Println(v)
+		}
+		for _, v := range dc.Removed {
+			fmt.Println(v)
+		}
+		return
 	}
 
-	fmt.Println("Remvoed lines")
-	for _, v := range dc.Removed {
-		fmt.Println("\t" + v)
+	if len(dc.Added) != len(dc.Removed) {
+		for _, v := range dc.Added {
+			fmt.Println(v)
+		}
+		for _, v := range dc.Removed {
+			fmt.Println(v)
+		}
+		return
 	}
+
+	var queue []string
+	for i := 0; i < len(dc.Added); i++ {
+		a, r := highlighPair(dc.Added[i], dc.Removed[i])
+		fmt.Println(r)
+		queue = append(queue, a)
+	}
+	for _, v := range queue {
+		fmt.Println(v)
+	}
+}
+
+func highlighPair(added, removed string) (string, string) {
+
+	addedRune, removedRune := []rune(added), []rune(removed)
+	seenPlusMinus := false
+	addedPrefixIndex, removedPrefixIndex := 0, 0
+	for addedPrefixIndex < len(addedRune) && removedPrefixIndex < len(removedRune) {
+		if addedRune[addedPrefixIndex] == removedRune[removedPrefixIndex] {
+			addedPrefixIndex++
+			removedPrefixIndex++
+		} else if !seenPlusMinus && removedRune[0] == '-' && addedRune[0] == '+' {
+			seenPlusMinus = true
+			addedPrefixIndex++
+			removedPrefixIndex++
+		} else {
+			break
+		}
+	}
+
+	addedSuffixIndex, removedSuffixIndex := len(addedRune)-1, len(removedRune)-1
+	for addedSuffixIndex > 0 && removedSuffixIndex > 0 {
+		if addedRune[addedSuffixIndex] == removedRune[removedSuffixIndex] {
+			addedSuffixIndex--
+			removedSuffixIndex--
+		} else {
+			break
+		}
+	}
+
+	return highlightLine(addedRune, addedPrefixIndex, addedSuffixIndex, "black:green"), highlightLine(removedRune, removedPrefixIndex, removedSuffixIndex, "black:red")
+}
+
+func highlightLine(line []rune, prefix, suffix int, theme string) string {
+	t := ansi.ColorCode(theme)
+	reset := ansi.ColorCode("reset")
+	return fmt.Sprint(string(line[:prefix]), t, string(line[prefix:suffix+1]), reset, string(line[suffix+1:]))
 }
