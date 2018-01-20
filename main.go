cd5deb23b3874474e1cf7ba8c8583143017474ac
kenshi kamata <kenshi.kamata@gmail.com>
2018-01-20 23:48:33 +0900

    もう少しマシな感じに

--- main.go
+++ main.go
@@ -10,16 +10,15 @@ import (
 	"strconv"
 	"strings"
 	"syscall"
-
-	"github.com/mgutz/ansi"
 )
 
 var (
-	ColorPattern     = regexp.MustCompile(`^\\x1b\[[0-9;]*m`)
-	TailColorPattern = regexp.MustCompile(`\\x1b\[[0-9;]*m$`)
-	HunkPattern      = regexp.MustCompile(`^(\\x1b\[[0-9;]*m)*\@\@`)
-	AddPattern       = regexp.MustCompile(`^(\\x1b\[[0-9;]*m)*\+`)
-	RemovedPattern   = regexp.MustCompile(`^(\\x1b\[[0-9;]*m)*-`)
+	headColorPattern   = regexp.MustCompile(`^\\x1b\[[0-9;]*m`)
+	tailColorPattern   = regexp.MustCompile(`\\x1b\[[0-9;]*m$`)
+	hunkPattern        = regexp.MustCompile(`^(\\x1b\[[0-9;]*m)*\@\@`)
+	addedLinePattern   = regexp.MustCompile(`^(\\x1b\[[0-9;]*m)*\+`)
+	removedLinePattern = regexp.MustCompile(`^(\\x1b\[[0-9;]*m)*-`)
+	nextHunkPattern    = regexp.MustCompile(`^(\\x1b\[[0-9;]*m)*[\@ ]`)
 )
 
 // DiffContext contains infomation about diff
@@ -31,19 +30,17 @@ type DiffContext struct {
 
 func main() {
 	signal.Ignore(syscall.SIGPIPE)
+
 	scanner := bufio.NewScanner(os.Stdin)
 	dc := &DiffContext{InHunk: false, Added: []string{}, Removed: []string{}}
+
 	for scanner.Scan() {
 		t := scanner.Text()
 		ul := UnescapedLine(t)
-		err := dc.handleLine(ul)
-		if err != nil {
-			log.Fatal(err)
-		}
+		dc.handleLine(ul)
 	}
 	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "reading standard input:", err)
-		return
+		log.Fatal(err)
 	}
 	dc.ShowHunk()
 }
@@ -54,52 +51,44 @@ func UnescapedLine(s string) string {
 	return strings.Trim(quoted, "\"")
 }
 
-func (dc *DiffContext) handleLine(input string) error {
+func (dc *DiffContext) handleLine(input string) {
 	if !dc.InHunk {
 		printQuotedLine(input)
-		dc.InHunk = HunkPattern.MatchString(input)
-	} else if isRemovedLine(input) {
-		dc.Removed = append(dc.Removed, input)
-	} else if isAddedLine(input) {
-		dc.Added = append(dc.Added, input)
-	} else {
-		dc.ShowHunk()
-		dc.Added = []string{}
-		dc.Removed = []string{}
-		b, err := regexp.MatchString(`^(\\x1b\[[0-9;]*m)*[\@ ]`, input)
-		if err != nil {
-			return err
-		}
-		dc.InHunk = b
+		dc.InHunk = hunkPattern.MatchString(input)
+		return
 	}
-	return nil
-}
-
-func isRemovedLine(in string) bool {
-	return RemovedPattern.MatchString(in)
-}
-
-func isAddedLine(in string) bool {
-	return AddPattern.MatchString(in)
+	if removedLinePattern.MatchString(input) {
+		dc.Removed = append(dc.Removed, input)
+		return
+	}
+	if addedLinePattern.MatchString(input) {
+		dc.Added = append(dc.Added, input)
+		return
+	}
+	dc.ShowHunk()
+	printQuotedLine(input)
+	dc.Added = []string{}
+	dc.Removed = []string{}
+	dc.InHunk = nextHunkPattern.MatchString(input)
 }
 
 // ShowHunk shows lien in dc
 func (dc *DiffContext) ShowHunk() {
 	if len(dc.Added) == 0 || len(dc.Removed) == 0 {
-		for _, v := range dc.Added {
+		for _, v := range dc.Removed {
 			printQuotedLine(v)
 		}
-		for _, v := range dc.Removed {
+		for _, v := range dc.Added {
 			printQuotedLine(v)
 		}
 		return
 	}
 
 	if len(dc.Added) != len(dc.Removed) {
-		for _, v := range dc.Added {
+		for _, v := range dc.Removed {
 			printQuotedLine(v)
 		}
-		for _, v := range dc.Removed {
+		for _, v := range dc.Added {
 			printQuotedLine(v)
 		}
 		return
@@ -107,7 +96,7 @@ func (dc *DiffContext) ShowHunk() {
 
 	var queue []string
 	for i := 0; i < len(dc.Added); i++ {
-		a, r := highlighPair(dc.Added[i], dc.Removed[i])
+		a, r := dc.highlighPair(dc.Added[i], dc.Removed[i])
 		printQuotedLine(r)
 		queue = append(queue, a)
 	}
@@ -121,49 +110,50 @@ func printQuotedLine(s string) {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(v)
+	_, err = fmt.Fprintln(os.Stdout, v)
+	if err != nil {
+		fmt.Println(s)
+		panic(err)
+	}
 }
 
-func highlighPair(added, removed string) (string, string) {
-
+func (dc *DiffContext) highlighPair(added, removed string) (string, string) {
 	seenPlusMinus := false
-	addedPrefixIndex, removedPrefixIndex := 0, 0
-	for addedPrefixIndex < len(added) && removedPrefixIndex < len(removed) {
-		if loc := ColorPattern.FindStringIndex(added[addedPrefixIndex:]); loc != nil {
-			addedPrefixIndex += loc[1]
-		} else if loc := ColorPattern.FindStringIndex(removed[removedPrefixIndex:]); loc != nil {
-			removedPrefixIndex += loc[1]
-		} else if added[addedPrefixIndex] == removed[removedPrefixIndex] {
-			addedPrefixIndex++
-			removedPrefixIndex++
-		} else if !seenPlusMinus && removed[removedPrefixIndex] == '-' && added[addedPrefixIndex] == '+' {
+	apIndex, rpIndex := 0, 0
+	for apIndex < len(added) && rpIndex < len(removed) {
+		if loc := headColorPattern.FindStringIndex(added[apIndex:]); loc != nil {
+			apIndex += loc[1]
+		} else if loc := headColorPattern.FindStringIndex(removed[rpIndex:]); loc != nil {
+			rpIndex += loc[1]
+		} else if added[apIndex] == removed[rpIndex] {
+			apIndex++
+			rpIndex++
+		} else if !seenPlusMinus && removed[rpIndex] == '-' && added[apIndex] == '+' {
 			seenPlusMinus = true
-			addedPrefixIndex++
-			removedPrefixIndex++
+			apIndex++
+			rpIndex++
 		} else {
 			break
 		}
 	}
 
-	addedSuffixIndex, removedSuffixIndex := len(added)-1, len(removed)-1
-	for addedSuffixIndex > addedPrefixIndex && removedSuffixIndex > removedPrefixIndex {
-		if loc := TailColorPattern.FindStringIndex(added[:addedSuffixIndex]); loc != nil {
-			addedPrefixIndex = loc[1] - 1
-		} else if loc := TailColorPattern.FindStringIndex(removed[:removedSuffixIndex]); loc != nil {
-			removedPrefixIndex = loc[1] - 1
-		} else if added[addedSuffixIndex] == removed[removedSuffixIndex] {
-			addedSuffixIndex--
-			removedSuffixIndex--
+	asIndex, rsIndex := len(added)-1, len(removed)-1
+	for asIndex > apIndex && rsIndex > rpIndex {
+		if loc := tailColorPattern.FindStringIndex(added[:asIndex]); loc != nil {
+			apIndex = loc[1] - 1
+		} else if loc := tailColorPattern.FindStringIndex(removed[:rsIndex]); loc != nil {
+			rpIndex = loc[1] - 1
+		} else if added[asIndex] == removed[rsIndex] {
+			asIndex--
+			rsIndex--
 		} else {
 			break
 		}
 	}
 
-	return highlightLine(added, addedPrefixIndex, addedSuffixIndex, "black:green"), highlightLine(removed, removedPrefixIndex, removedSuffixIndex, "black:red")
+	return getHighlightedLine(added, apIndex, asIndex), getHighlightedLine(removed, rpIndex, rsIndex)
 }
 
-func highlightLine(line string, prefix, suffix int, theme string) string {
-	t := ansi.ColorCode(theme)
-	reset := ansi.ColorCode("reset")
-	return fmt.Sprint(line[:prefix], t, line[prefix:suffix+1], reset, line[suffix+1:])
+func getHighlightedLine(line string, prefix, suffix int) string {
+	return fmt.Sprint(line[:prefix], "\x1b[7m", line[prefix:suffix+1], "\x1b[27m", line[suffix+1:])
 }
